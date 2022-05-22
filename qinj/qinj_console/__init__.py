05b33228ce68c8989585b30a57ee9d6a030cc5cc
Saul St John <saul.stjohn@gmail.com>
2022-05-22 01:03:39 -0500

    add message_box utility

--- qinj/qinj_console/__init__.py
+++ qinj/qinj_console/__init__.py
@@ -3,5 +3,17 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
+def message_box(text = "Hello, world!\n\nPress MFK to dismiss."):
+	top_level_widget = QApplication.topLevelWidgets()[-1]
+	
+	#what is the correct way to do this?
+	injection = top_level_widget.children()[-3]
+
+	QMetaObject.invokeMethod(
+		injection,
+		"messageBoxRequested",
+		Q_ARG(str, text)
+	)
+
 def run():
 	IPython.embed(header="qinj console")
